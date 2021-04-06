0b4c46c61784751d263c5ec443fa6fc0882d0681
sarawrotniak <sara.wrotniak@uconn.edu>
2021-04-06 01:08:54 -0400

    show more info

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Analyze data from a HAR file.
 - Replace the file `harfile` in the downloaded project with your captured HAR file (or copy the contents into it). Make sure it's called `harfile`
 - Make sure to have `Node.js` installed (version>=10.13.x)
 - Install the `Objectron` module with: `npm install @menadevs/objectron`
-- Change into the directory of this project. Run the program with: `node .\makePage.js`. An HTML file named `results.html` should be created in the same directory
+- Change into the directory of this project. Run the program with: `node makePage.js`. An HTML file named `results.html` should be created in the same directory
 - Open the `results.html` file with a browser (Chrome, FireFox, ...)
 - Analyze what you see
--- makePage.js
+++ makePage.js
@@ -12,40 +12,51 @@ try {
 }
 
 var html = htmlTop;
-html += "<h1>URLs of All Requests</h1>";
-html += "<h1 style='font-size:120%;''>and method and URL parameters</h1>";
+html += "<h1>Info About Requests</h1>";
+html += "<p> <span style='color:black;'>Below are shown the type of request (GET/POST), any parameters sent in the URL (in blue), and the names of any cookies sent (in green). Also, origin and referer may be shown in blue if present. </span><\p>";
 
-const interestingStuffFilter = {
+const requestFilter = { "request": (val) => val };
+
+const ORFilter = {
     "request": {
-        "method": (val) => val,
-        "url": (val) => val
-    }
-};
-
-var numMatches = 0;
+        "headers": [
+            {   "name": "Origin",
+                "value": (val) => val },
+            {   "name": "Referer",
+                "value": (val) => val } ,
+            {   "name": "origin",
+                "value": (val) => val },
+            {   "name": "referer",
+                "value": (val) => val } ] } }
 
 for (var i=0; i<HARdata.log.entries.length; i++) {
-    var result = match(HARdata.log.entries[i], interestingStuffFilter);
+    var entry = HARdata.log.entries[i];
+    var result = match(entry, requestFilter);
+    var result2 = match(entry, ORFilter);
     if (result.match == true) {
-        numMatches++;
-        var url = result.matches.request.url;
+        var url = entry.request.url; //url
         html += "<h3>" + url + "<\h3>";
-        html += "<p> <span style='color:white;'>" + result.matches.request.method + "</span><\p>";
-        var paramIndex = url.indexOf("?");
-        if (paramIndex != -1) {
-            html += "<p><span style='color:white;'>URL params:</span><\p><p>";
-            for (var j=paramIndex+1; j<url.length; j++) {
-                if (url[j]=='&') html += "<\p><p>";
-                else html += url[j];
-            }
-            html += "<\p>";
+        html += "<p> <span style='color:black;'>" + entry.request.method + "</span><\p>"; //GET/POST
+        //origin, referer
+        for (var j=0; j<4; j++)
+            if (result2.matches.request.headers[j].name)
+                html += "<p> " + result2.matches.request.headers[j].name + ": "
+                    + result2.matches.request.headers[j].value + "<\p>";
+        //queryString
+        if (entry.request.queryString.length>0) {
+            html += "<p> <span style='color:black;'>" + "queryString (URL params):" + "</span><\p>";
+            for (var j=0; j<entry.request.queryString.length; j++)
+                html += "<p> " + entry.request.queryString[j].name + ": " + entry.request.queryString[j].value + "<\p>";
+        }
+        //names of cookies
+        if (entry.request.cookies.length>0) {
+            html += "<p> <span style='color:black;'>" + "names of cookies:" + "</span><\p>";
+            for (var j=0; j<entry.request.cookies.length; j++)
+                html += "<p> <span style='color:green;'>" + entry.request.cookies[j].name + "</span><\p>";
         }
     }
 }
 
-console.log(HARdata.log.entries.length);
-console.log(numMatches);
-
 html += (htmlEnd);
 
-fs.writeFile('results.html', html, (err) => { if (err) throw err; })
+fs.writeFile('results.html', html, (err) => { if (err) throw err; });
